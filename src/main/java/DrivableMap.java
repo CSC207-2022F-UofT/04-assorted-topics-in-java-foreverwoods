e17765373f3145380759a032313bd5635ad0f6e7
woods <j2hashz0p@outlook.com>
2022-10-09 23:38:36 -0400

    mans done why was this way easier than exercise 3

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -7,6 +7,7 @@
  * created the constructor for you already.
  */
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,9 +28,14 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
-
-
-
+        public boolean addDrivable(String ID, Drivable object){
+            if(drivable_map.containsKey(ID)){
+                return false;
+            }
+            else
+                drivable_map.put(ID, object);
+                return true;
+        }
 
     /* TODO: Write a method named hasFasterThan that takes an int (a speed)
      *       and returns true iff there is at least one item in drivable_map
@@ -37,16 +43,28 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
-
-
-
-
+        public boolean hasFasterThan(int speed){
+            for(Drivable item: drivable_map.values()){
+                if(item.getMaxSpeed() >= speed){
+                    return true;
+                }
+            }
+            return false;
+        }
 
     /* TODO: Write a method named getTradable that takes no arguments and
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
-
+        public List<Tradable> getTradable(){
+            ArrayList<Tradable> items = new ArrayList<Tradable>();
+            for(Drivable item : this.drivable_map.values()){
+                if (item instanceof Tradable){
+                    items.add((Tradable)item);
+                }
+            }
+            return items;
+        }
 
 
     
--- src/main/java/DrivableTrader.java
+++ src/main/java/DrivableTrader.java
@@ -11,3 +11,34 @@
  */
 
 import java.util.List;
+
+class DrivableTrader extends Trader<Drivable>{
+    /**
+     * Construct a DrivableTrader, giving them the given inventory,
+     * wishlist, and money.
+     *
+     * @param inventory Objects in this Trader's inventory
+     * @param wishlist  Objects in this Trader's wishlist
+     * @param money     The Trader's money
+     */
+
+    public DrivableTrader(List<Drivable> inventory, List<Drivable> wishlist,
+                  int money) {
+        super(inventory, wishlist, money);
+    }
+
+    public DrivableTrader(int money){
+        super(money);
+    }
+
+    @Override
+    public int getSellingPrice(Drivable object){
+        int super_price = super.getSellingPrice(object);
+
+        if (object instanceof Tradable){
+            return(super_price + object.getMaxSpeed());
+        }
+        else
+            return Tradable.MISSING_PRICE;
+    }
+}
